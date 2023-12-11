2406d794b0c6cd483feaedfe9d045347aa9b1446
unknown <ivaylogi@outlook.com>
2023-12-12 00:34:49 +0200

    progress

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,49 +1,50 @@
-'use client';
-
 import { useState } from "react";
 import './chat.css';
 import ChatMessages from "../messages/ChatMessages";
-import { doc, updateDoc, arrayUnion, onSnapshot, Timestamp, collection, setDoc, getDoc } from "firebase/firestore";
+import { doc, updateDoc, arrayUnion, Timestamp, setDoc, getDoc, DocumentData, getFirestore } from "firebase/firestore";
 import { db } from "../../firebase/config";
 import { useSelector } from "react-redux";
-import { GetStoreData, ChatInpData, CurrentUserImpl } from "../../public/interfaces/globals";
+import { GetStoreData } from "../../public/interfaces/globals";
 import { v4 as uuid } from "uuid";
-import { useEffect } from "react";
-import { getUserData, currUserData } from "../../app/api/helper/users/userService";
+import { currUserData } from "../../app/api/helper/users/userService";
 import { usePathname } from "next/navigation";
+import { UserImpl } from "../../app/redux/store";
 
-const Chat = () => {
+const fetchchatUsernameseData = async () => {
+    const path = usePathname();
+    //Get everything after userChat/ which is the user ID
+    const regex = /(?<=userChat\/).*/gm;
+    const userID: string = regex.exec(path)![0];
+    const firestoreChatRef = doc(db, 'chatUsernames', userID);
+    const chatRefSnap = await getDoc( firestoreChatRef );
+    const data = await chatRefSnap.data();
+    return data;
+}
+
+const fetchChatData = async (combinedId: string) => {
+    const firestoreChatRef = doc(db, 'chats', combinedId);
+    const chatRefSnap = await getDoc( firestoreChatRef );
+    const data = await chatRefSnap.data();
+    return data;
+}
+
+const Chat = async () => {
+    // const combinedId: any = useSelector<GetStoreData>( state => state.dataStore.combinedId );
     const [ chatInput, setChatInput ] = useState('');
-    const [ userData, setUserData ]: any[] = useState();
+    // const [ userData, setUserData ]: any[] = useState();
     const [ currentUserUID, setCurrentUserUID ]: any = useState();
-    const [ userChatClicked, setUserChatClicked ] = useState(false);
-    const [ userChatNames, setUserChatNames ]: any = useState([]);
-    const [ selectedUserMessages, setSelectedUserMessages ] = useState([]);
+    const [ myChatMessages, setMyChatMessages ] = useState('');
+    const [ selectedUserChatMsgs, setSelectedUserChatMsgs ] = useState('');
     const combinedId: any = useSelector<GetStoreData>( state => state.dataStore.combinedId );
     const chatNames: any = useSelector<GetStoreData>( state => state.dataStore.userChatNames );
-    const path = usePathname();
-
-    useEffect( () => {
-        currUserData().then( data => setUserData(data[0]) );
-        //Get everything after userChat/ which is the user ID
-        const regex = /(?<=userChat\/).*/gm;
-        const userID: string = regex.exec(path)![0];
-        setCurrentUserUID(userID);
-
-        const chatNamesFunc = async () => {
-            const firestoreChatRef = doc(db, 'chatUsernames', userID);
-            const chatRefSnap = await getDoc( firestoreChatRef );
-            const data = chatRefSnap.data();
-            setUserChatNames(data?.names);
-        }
-        
-        chatNamesFunc();
-        // console.log(userChatNames);
-    }, [ path, chatNames ] );
-	
+    const chatNamesData = await fetchchatUsernameseData();
+    const chatData = await fetchChatData(combinedId);
+    console.log(chatData);
+    	
     const handleEnter = async (event: any) => {
-        const senderUid: string = userData.uid;
         if ( event.key === 'Enter' && event.target.value !== '' ) {
+            const userData: UserImpl[] = await currUserData();
+            const senderUid: string = userData[0].uid;
             setChatInput(event.target.value);
             await updateDoc(doc(db, 'chats', combinedId), {
                 messages: arrayUnion({
@@ -63,20 +64,19 @@ const Chat = () => {
     const startChat = async () => {
         const firestoreChatRef = doc(db, 'chats', combinedId);
         const chatRefSnap = await getDoc( firestoreChatRef );
-        console.log(chatRefSnap.data());
+        const data: DocumentData | undefined = chatRefSnap.data();
     }
-    const mapedChatNames = userChatNames && userChatNames.map( (name: string) => <p onClick={startChat} key={uuid()}>{name}</p> );
 
     return (
         <div className="chat-wrapper m-auto mt-24 border border-black rounded">
             <div className='bg-red-300 p-5'></div>
             <div className="chat-inner flex h-full">
                 <div className="w-36 bg-white h-full border-r border-black">
-                    { mapedChatNames }
+                    { chatNamesData?.names.map( (name: string) => <p key={uuid()}>{name}</p> ) }
                 </div>
-                <div className={`w-full ${userChatClicked ? 'bg-white' : 'bg-gray-200'} relative`}>
+                <div className={`w-full 'bg-white' relative`}>
                     <div className="h-full bg-yellow-100 p-5 overflow-auto overflow-y-scroll">
-                        <ChatMessages messages={chatInput} />
+                        <ChatMessages myMsgs={myChatMessages} userMsgs={selectedUserChatMsgs} />
                     </div>
                     <div className='flex justify-between items-center bg-white absolute bottom-0 w-full border-1 border-black'>
                         <input onKeyDown={handleEnter} type="text" placeholder='Изпрати съобщение' className='w-full py-8 px-5 outline-none' />
--- components/messages/ChatMessages.tsx
+++ components/messages/ChatMessages.tsx
@@ -4,35 +4,22 @@ import { db } from "../../firebase/config";
 import { PassedMessages } from "../../public/interfaces/globals";
 import { v4 } from 'uuid';
 
-const ChatMessages = (props: any) => {
+const ChatMessages = ({myMsgs, userMsgs}: { myMsgs: string; userMsgs: string }) => {
     const [ personalText, setPersonalText ] = useState(['']);
 
     useEffect( () => {
-        // const res = onSnapshot(doc(db, "chats", props.combinedId ), (doc: any) => {
-        //     const textData: PassedMessages = doc.data();
-        //     // textData.messages.forEach( data => setPersonalText( prevText => [ ...personalText, data.text ] ));
-        // });
-
-        // console.log(props.selectedUserMsgs);
-        //unsub
-        // return () => {
-        //     res();
-        // }
         
     }, [ ]);
-
-    // const chatElem = props.selectedUserMsgs.map( (item: any) => <p key={item.id + v4()} className="bg-blue-300 my-2 p-3 rounded-xl">{item.text}</p> );
 
     return (
         <div className="messageContainer">
             <div className="w-full mb-5">
-                <div className="bg-white w-40 text-center py-3 text-xl rounded-lg">Your message</div>
+                <div className="bg-white w-40 text-center py-3 text-xl rounded-lg">Your message {userMsgs}</div>
             </div>
 
             <div className="w-full">
                 <div className="text-center text-white text-xl rounded-lg float-right">
-                    {/* {chatElem} */}
-                    <p>static for now</p>
+                    <p>My message{myMsgs}</p>
                 </div>
             </div>
         </div>
